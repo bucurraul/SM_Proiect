6102a33b2153ed523ea6b107eae146112be7447f
Bucur Raul <raul-vasile.bucurTUIASI@student.tuasi.ro>
2020-06-07 01:37:53 +0300

    QWE

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,14 @@
 
 function updateDegreesBox(value) {
     $('#degrees_box').val(value);
-
+    console.log("Mesaj")
     $.ajax({
         type: 'POST',
         url: '/servo',
         data: {
             'degrees': $('#degrees').val()
         },
+	success: () => { console.log("Lambda OK")},
     });
 }
 
